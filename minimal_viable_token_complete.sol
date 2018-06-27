2d6036aa5ac1ae46cbc0bde63ad83647f4b10bc5
kevin11h <kevhung11@gmail.com>
2018-06-27 11:30:55 -0700

    more complete token code

---
+++ minimal_viable_token_complete.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.16;
+
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extractData) { external; }
+
+
+
+
+
+
+
+
+
+}
